15f35548a5bbb023d06747c8b69f24e616fd8f90
Your Name <your-email@example.com>
2025-12-24 12:24:31 -0600

    Update AwsConfig with Cognito

--- mobile/src/services/awsConfig.js
+++ mobile/src/services/awsConfig.js
@@ -1,16 +1,16 @@
 const awsConfig = {
   Auth: {
-    region: "YOUR_AWS_REGION",
-    userPoolId: "YOUR_USER_POOL_ID",
-    userPoolWebClientId: "YOUR_USER_POOL_APP_CLIENT_ID",
+    region: "us-east-2",
+    userPoolId: "us-east-2_i5aZjCPUZ",
+    userPoolWebClientId: "49nc8hor19c1lb0ppog52hql5i",
     oauth: {
-      domain: "YOUR_COGNITO_DOMAIN",
-      scope: ["email", "openid", "profile"],
-      redirectSignIn: "ai17-outreach://auth",
-      redirectSignOut: "ai17-outreach://signout",
-      responseType: "code"
-    }
-  }
+      domain: "us-east-2i5azjcpuz.auth.us-east-2.amazoncognito.com",
+      scope: ["openid", "email", "profile"],
+      redirectSignIn: "https://auth.expo.io/@kettleson1/ai17-outreach-hq",
+      redirectSignOut: "https://auth.expo.io/@kettleson1/ai17-outreach-hq",
+      responseType: "code",
+    },
+  },
 };
 
 export default awsConfig;
