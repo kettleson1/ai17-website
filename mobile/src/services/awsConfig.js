dc84451a5a9b7814e226c2c004155782d314b9bb
Your Name <your-email@example.com>
2025-12-29 15:13:41 -0600

    Update Cognito Awsconfig

--- mobile/src/services/awsConfig.js
+++ mobile/src/services/awsConfig.js
@@ -3,7 +3,7 @@ const awsConfig = {
     region: "us-east-2",
     userPoolId: "us-east-2_i5aZjCPUZ",
     userPoolWebClientId: "49nc8hor19c1lb0ppog52hql5i",
-    identityPoolId: "us-east-2:YOUR-IDENTITY-POOL-ID",
+    identityPoolId: "us-east-2:ee691469-7a28-423a-ad8b-42c300566ec2",
     oauth: {
       domain: "us-east-2i5azjcpuz.auth.us-east-2.amazoncognito.com",
       scope: ["openid", "email", "profile"],
